41a9e46c07f44dc67a5404114d980c56df32f57e
Ahmed Jamal Tariq <40691417+ahmedjamaltariq@users.noreply.github.com>
2018-06-30 01:04:12 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,12 @@
 <?php
-//namespaces + inheritance + abstraction + autoloading
-require 'Helper/autoload.php';
+//first of all extract the PHPMagicLoader.zip in the root directory of your project and then require it in your index.php class
+require 'PHPMagicLoader/autoload.php';
 
-
+//the create its instance and pass the name of file which you want to load and it must be present anywhere in root diretory or its sub direcotries
+//As an example it is loading 'company.php' which is present anywhere in root directory
 
 $auto = new Autoloader('company.php');
 require($auto->path);
-
-
-
-
-
-$auto = new Autoloader('employees.php');
-require($auto->path);
-
-
-$newEmp = new Karigar\App\Employees();
-$newEmp->CompanyName();
-
-
 
 
   ?>
