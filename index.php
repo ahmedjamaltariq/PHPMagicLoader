84f62b2151b017c0bc39295e5faa33a25b1b14fe
Ahmed Jamal Tariq <40691417+ahmedjamaltariq@users.noreply.github.com>
2018-07-01 02:40:16 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 //first of all extract the PHPMagicLoader.zip in the root directory of your project and then require it in your index.php class
 require 'PHPMagicLoader/autoload.php';
 
-//the create its instance and pass the name of file which you want to load and it must be present anywhere in root diretory or its sub direcotries
-//As an example it is loading 'company.php' which is present anywhere in root directory
+$libraries = new Autoloader('employees.php|company.php');
+foreach ($libraries->paths as $library) { require($library);}
 
-$auto = new Autoloader('company.php');
-require($auto->path);
+//now the all the libraries have been loaded in your project, you can use them.
+$newEmp = new Karigar\App\Employees();
 
 
   ?>
