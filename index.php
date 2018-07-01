5d3bcb3788c3d13e1abbc133cdb2321dc369cc8d
Ahmed Jamal Tariq <40691417+ahmedjamaltariq@users.noreply.github.com>
2018-07-01 13:47:43 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 //first of all extract the PHPMagicLoader.zip in the root directory of your project and then require it in your index.php class
 require 'PHPMagicLoader/autoload.php';
 
-$libraries = new Autoloader('employees.php|company.php');
+$libraries = new MagicLoad('employees.php|company.php');
 foreach ($libraries->paths as $library) { require($library);}
 
 //now the all the libraries have been loaded in your project, you can use them.
